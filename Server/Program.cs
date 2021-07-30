d529c9e7c721957839e0e35a83c1dbd0a5532b05
牧瀬紅莉栖 <MakiseKurisu@users.noreply.github.com>
2021-07-30 22:23:19 +0800

    Add basic command line handling

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,27 +1,40 @@
 ﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace pdi.Server
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task<int> Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+            var daemon = new Command("daemon", "Start web daemon");
+            daemon.Handler = CommandHandler.Create(async (CancellationToken token) =>
+            {
+                await Host.CreateDefaultBuilder()
+                    .ConfigureWebHostDefaults(webBuilder =>
+                    {
+                        webBuilder.UseStartup<Startup>();
+                    })
+                    .Build()
+                    .RunAsync(token);
+            });
+
+            var show = new Command("show", "Show cluster information");
+
+            var root = new RootCommand("Proton Distributed Infrastructure");
+            root.Add(daemon);
+            root.Add(show);
+
+            // Parse the incoming args and invoke the handler
+            return await root.InvokeAsync(args);
+        }
     }
 }
