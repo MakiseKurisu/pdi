4342b11bfda5ebfa2bd017003db715fdb3ea6805
牧瀬紅莉栖 <MakiseKurisu@users.noreply.github.com>
2021-08-05 19:23:16 +0800

    Add test for Host.Connect

--- pdi.Tests/pdi/Asset/HostTests.cs
+++ pdi.Tests/pdi/Asset/HostTests.cs
@@ -1,6 +1,4 @@
-﻿using pdi.Asset;
-
-using System;
+﻿using Microsoft.Extensions.Configuration;
 
 using Xunit;
 
@@ -19,6 +17,37 @@ namespace pdi.Asset.Tests
             Assert.Null(i.Password);
             Assert.Null(i.PrivateKey);
             Assert.Null(i.PrivateKeyPassword);
+        }
+
+        [Fact]
+        public void ConnectPasswordTest()
+        {
+            var secrets = new ConfigurationBuilder().AddUserSecrets<HostTests>().Build();
+
+            var i = new Host()
+            {
+                Address = secrets["SSH_ADDRESS"],
+                UserName = secrets["SSH_USERNAME"],
+                Password = secrets["SSH_PASSWORD"]
+            };
+
+            i.Connect();
+        }
+
+        [Fact(Skip = "Current private key is not supported in SSH.NET 2020.0.1. See https://github.com/sshnet/SSH.NET/pull/614")]
+        public void ConnectPrivateKeyTest()
+        {
+            var secrets = new ConfigurationBuilder().AddUserSecrets<HostTests>().Build();
+
+            var i = new Host()
+            {
+                Address = secrets["SSH_ADDRESS"],
+                UserName = secrets["SSH_USERNAME"],
+                PrivateKey = secrets["SSH_PRIVATE_KEY"],
+                PrivateKeyPassword = secrets["SSH_PRIVATE_KEY_PASSWORD"]
+            };
+
+            i.Connect();
         }
     }
 }
