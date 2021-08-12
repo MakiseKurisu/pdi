a9e642648000a5271b74ece76050695b2b7f28d3
牧瀬紅莉栖 <MakiseKurisu@users.noreply.github.com>
2021-08-12 15:26:44 +0800

    Add HostFactory

---
+++ pdi.Tests/Assets/HostFactoryTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+
+using System;
+
+using Xunit;
+
+namespace pdi.Assets.Tests
+{
+    public class HostFactoryTests
+    {
+        [Fact]
+        public async void ShellDetectionTest()
+        {
+            var secrets = new ConfigurationBuilder().AddUserSecrets<SshHostTests>().Build();
+
+            var i = new SshHostRecord()
+            {
+                Address = secrets["SSH_ADDRESS"],
+                Port = Convert.ToInt32(secrets["SSH_PORT"]),
+                UserName = secrets["SSH_USERNAME"],
+                Password = secrets["SSH_PASSWORD"]
+            };
+
+            using var s = new SshHost(i);
+            var f = new HostFactory();
+            Assert.Equal(HOST_SHELL_TYPE.SH, await f.ShellDetection(s));
+        }
+    }
+}
---
+++ pdi/Assets/HostFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pdi.Assets
+{
+    public enum HOST_SHELL_TYPE
+    {
+        SH,
+        POWERSHELL,
+        CMD,
+    }
+
+    public class HostFactory
+    {
+        public async Task<HOST_SHELL_TYPE> ShellDetection(SshHost Host)
+        {
+            (var ExitStatus, var Result, _) = await Host.Execute("echo $?");
+            
+            if (ExitStatus != 0)
+            {
+                throw new PlatformNotSupportedException("Unknown Shell.");
+            }
+
+            return Result switch
+            {
+                "0\n"       => HOST_SHELL_TYPE.SH,
+                "True\n"    => HOST_SHELL_TYPE.POWERSHELL,
+                "$?\n\n"    => HOST_SHELL_TYPE.CMD,
+                _           => throw new PlatformNotSupportedException("Unknown Shell.")
+            };
+        }
+    }
+}
